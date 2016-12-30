863b6922baeaf7da909227d4a98248701bc204c5
Derek Carlson <carlson.derek@gmail.com>
2016-12-29 17:50:17 -0800

    Add wrapper to boot.rb to clock and display load times of all gems.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,18 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require "benchmark"
+
+def require(file_name)
+  result = nil
+
+  time = Benchmark.realtime do
+    result = super
+  end
+
+  if time > 0.1
+    puts "#{time} #{file_name}"
+  end
+
+  result
+end
