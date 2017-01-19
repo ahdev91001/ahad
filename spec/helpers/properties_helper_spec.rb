65e84e1e3579b812b0d687eaedc89b2dfdc5bc82
Derek Carlson <carlson.derek@gmail.com>
2017-01-19 19:35:22 +0000

    Copy properties_helper_spec from draper branch.

--- spec/helpers/properties_helper_spec.rb
+++ spec/helpers/properties_helper_spec.rb
@@ -41,37 +41,50 @@ RSpec.describe "PropertiesHelper", :type => :helper do
         .to eq("259 ACACIA ST")
     end
 
-    it "turns ST. and STREET into ST" do
-      expect(normalize_address("259 ACACIA ST."))
-        .to eq("259 ACACIA ST")
-      expect(normalize_address("259 ACACIA STREET"))
-        .to eq("259 ACACIA ST")
-    end
+    expected_values = {
+      '259 ACACIA ST.': '259 ACACIA ST', 
+      '259 ACACIA STREET': '259 ACACIA ST',
+      '1437 ALPHA AVE.': '1437 ALPHA AVE', 
+      '1437 ALPHA AVENUE': '1437 ALPHA AVE',
+      '3544 CANON BLVD.': '3544 CANON BLVD', 
+      '3544 CANON BOULEVARD': '3544 CANON BLVD',
+      '3705 ALZADA RD.': '3705 ALZADA RD',
+      '3705 ALZADA ROAD': '3705 ALZADA RD',
+      '2115 ALTA PASA DR.': '2115 ALTA PASA DR', 
+      '2115 ALTA PASA DRIVE': '2115 ALTA PASA DR',
+      '607 BARRY PL.': '607 BARRY PL', 
+      '607 BARRY PLACE': '607 BARRY PL',
+      '3260 ALEGRE LN.': '3260 ALEGRE LN', 
+      '3260 ALEGRE LANE': '3260 ALEGRE LN',
+      '1314 SUNNY OAKS CIR.': '1314 SUNNY OAKS CIR', 
+      '1314 SUNNY OAKS CIRCLE': '1314 SUNNY OAKS CIR',
+      '1277 AVOCADO TER.': '1277 AVOCADO TER', 
+      '1277 AVOCADO TERRACE': '1277 AVOCADO TER',
+      '2756 BULA CT.': '2756 BULA CT', 
+      '2756 BULA COURT': '2756 BULA CT',
+      '3430 CHANEY TR.': '3430 CHANEY TR', 
+      '3430 CHANEY TRAIL': '3430 CHANEY TR',
+      '1080 BEVERLY WY.': '1080 BEVERLY WAY', 
+      '1080 BEVERLY WY': '1080 BEVERLY WAY',
+      
+      '1367 NORTH ALTADENA DR': '1367 N ALTADENA DR', 
+      '92 EAST HARRIET ST': '92 E HARRIET ST', 
+      '419 WEST PALM ST': '419 W PALM ST',
+      '111 SOUTH FICTITIOUS LN': '111 S FICTITIOUS LN',
 
-    it "turns AVE. and AVENUE into AVE" do
-      expect(normalize_address("259 ACACIA AVE."))
-        .to eq("259 ACACIA AVE")
-      expect(normalize_address("259 ACACIA AVENUE"))
-        .to eq("259 ACACIA AVE")
-    end
+      '1000 E MOUNT CURVE AVE': '1000 E MT CURVE AVE', 
+      '1099 MOUNT LOWE DR': '1099 MT LOWE DR' # no comma
+    }
+    
+    expected_values.each do |val, expected| 
+      it "returns #{expected} when input is #{val}" do 
+        expect(normalize_address(val.to_s)).to eq(expected)
+      end
+    end  
 
-    it "turns BLVD. and BOULEVARD into BLVD" do
-      expect(normalize_address("259 ACACIA BLVD."))
-        .to eq("259 ACACIA BLVD")
-      expect(normalize_address("259 ACACIA BOULEVARD"))
-        .to eq("259 ACACIA BLVD")
-    end
-
-  # TODO: Complete for the rest of these, and clean up data
-  # inconsistencies in dev and prod and real prod db
-  #
-  # Rd,Dr,Pl,Way,Ln,Cir,Ter
-  # Ct,Blvd,South,Palmyra,Tr,Wy
-  # DONE: Blvd, Ave, St, 
-
-    it "passes a wacky contrived test of all of the above" do
-      expect(normalize_address("  \t259  \tAcaCia   st.   \t  "))
-        .to eq("259 ACACIA ST")
+    it "passes a wacky contrived whitespace test of a mix of the above" do
+      expect(normalize_address("  \t259 EAST \tAcaCia   st.   \t  "))
+        .to eq("259 E ACACIA ST")
     end
 
   end # #normalize_address
