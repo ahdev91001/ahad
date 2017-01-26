ae6d059542ac7750bae248adb13346f58f10b084
Derek Carlson <carlson.derek@gmail.com>
2017-01-26 22:55:27 +0000

    Remove testsqlite.rb file, as well as its secret in secrets.yml.  Comment out benchmark code in boot.rb.

--- config/boot.rb
+++ config/boot.rb
@@ -5,8 +5,9 @@ require 'bundler/setup' # Set up gems listed in the Gemfile.
 # Uncomment below to get load times for all gems when rails boots
 # Code from: http://mildlyinternet.com/code/profiling-rails-boot-time.html
 require "benchmark"
-require "pry-byebug"  # causes Heroku to crash on install
 
+=begin  # :rake-automated-do-not-remove-this
+require "pry-byebug"  # causes Heroku to crash on install
 
 def require(file_name)
   result = nil
@@ -26,3 +27,4 @@ def require(file_name)
 
   result
 end
+=end  # :rake-automated-do-not-remove-this
