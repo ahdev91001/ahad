6c179f35512fa18e4b2bec59563a8dc4f1d96166
Derek Carlson <carlson.derek@gmail.com>
2018-10-20 03:46:05 +0000

    Added PDF download link, and pdf format processor in the controller, which creates a test.pdf file that downloads.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -33,6 +33,16 @@ class PropertiesController < ApplicationController
   def show
     source = Property.find(params[:id])
     @property = PropertyDecorator.new(source)
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = Prawn::Document.new
+        pdf.text 'Hello World'
+  
+        send_data pdf.render        
+      end
+    end
   end
 
 end
