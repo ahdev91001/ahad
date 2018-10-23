92fee6d37db668f66c00e00744f96da1a0ce39f4
Derek Carlson <carlson.derek@gmail.com>
2018-10-23 16:38:02 +0000

    Users#create specs all pass, including uniqueness.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,95 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe '#email' do
+  describe '#create' do
   
-    it 'validates presence' do
-      record = User.new
-      #record.email = '' # invalid state
-      #record.valid? # run validations
-      #record.errors[:email].should include("can't be blank") # check for presence of error
+    before(:each) { @user = User.new(name: "Bob", email: "flammy@zoo.com", password: "foobar", password_confirmation: "foobar") }
 
-      record.email = 'foo@bar.com' # valid state
-      record.valid? # run validations
-      record.errors[:email].should_not include("can't be blank") # check for absence of error
+    it "is valid with all proper parameters supplied" do
+      expect(@user).to be_valid
     end
-    
-  end
 
+    it "is not valid with a missing email" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with an email consisting of only spaces" do
+      @user.email = "   "
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with a malformed email" do
+      @user.email = "flammy.at.com"
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a name" do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with a name consisting of only spaces" do
+      @user.name = "   "
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid if missing a password" do
+      @user.password = nil
+      @user.password_confirmation = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid if password just has spaces" do
+      @user.password = " " * 10
+      @user.password_confirmation = " " * 10
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with a password with too few characters" do
+      @user.password = "foo"
+      @user.password_confirmation = "foo"
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid if password and confirmation do not match" do
+      @user.password = "raul"
+      @user.password_confirmation = "charles"
+      expect(@user).to_not be_valid
+    end
+
+    it "is invalid if password has too few characters" do
+      @user.password = "a" * 5
+      expect(@user).to_not be_valid
+    end
+
+    it "is invalid if a name is too long" do
+      @user.name = "a" * 51
+      expect(@user).to_not be_valid
+    end
+
+    it "is valid if a name is not too long" do
+      @user.name = "a" * 50
+      expect(@user).to be_valid
+    end
+
+    it "is invalid if an email is too long" do
+      @user.name = "a" * 255 + "@fred.com"
+      expect(@user).to_not be_valid
+    end
+
+    it "should not allow duplicate email addresses" do
+      @userdup = User.new(name: "Ted", email: "flammy@zoo.com", password: "mrfoobar", password_confirmation: "mrfoobar")
+      @user.save
+      expect(@userdup).to_not be_valid
+    end
+
+    it "should not allow duplicate email addresses even if their case is different" do
+      @userdup = User.new(name: "Sam", email: "FlamMY@ZOO.com", password: "mrfoobar", password_confirmation: "mrfoobar")
+      @user.save
+      expect(@userdup).to_not be_valid
+    end
+
+
+  end
 end
