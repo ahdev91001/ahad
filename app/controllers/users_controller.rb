d89af8c7e46451e7c9737ec5b2085e9b4e734808
Derek Carlson <carlson.derek@gmail.com>
2018-10-23 23:13:32 +0000

    Finish user signup page, except have not written any tests.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Your account has been created! Welcome to AHAD you wonderful person you!"
+      flash.keep # not even sure why this is needed, but it was
+      # second answer at: https://stackoverflow.com/questions/7510418/
+      #                     rails-redirect-to-with-error-but-flasherror-empty
+      redirect_to user_url(@user) 
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,13 @@ Rails.application.routes.draw do
   # resources :properties
   
   root 'static_pages#home'
-
   get 'static_pages/help'
-
   get 'static_pages/testform'
-  
+  get 'static_pages/railstutorial'
   get '/search' => 'static_pages#search' 
-
   get 'properties/:id' => 'properties#show', as: 'property'
-  
   get 'properties' => 'properties#index', as: 'properties'
-
   get  '/signup',  to: 'users#new'
-
+  post '/signup',  to: 'users#create'
+  resources :users
 end
