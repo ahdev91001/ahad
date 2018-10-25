083daf07b81c8f7311dbc395658b8913595db72a
Derek Carlson <carlson.derek@gmail.com>
2018-10-25 17:15:13 +0000

    Added basic menu bar.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -68,25 +68,7 @@ var DEBUG = true;  // "const" doesn't work in IE 8, 9, 10
 $(document).on('ready', function (e) {
   if (DEBUG) console.log("Event: ready() at: " + e.timeStamp);
 
-  $(function() {
-    $('nav ul li > a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-      e.stopPropagation();
-    });
-  });
-  document.querySelector('#nav-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-  });
-  $('#nav-toggle').click(function() {
-    $('nav ul').toggle();
-    e.stopPropagation();
-  });
-  
+
 });
 
 
