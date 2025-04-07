16a8e1eebc779d57b54fb296c45c2e2c16e44346
Cole Meyers <colemeyers27@gmail.com>
2025-04-07 15:22:21 -0700

    burned down postgresql RoR app; recreated RoR with --database=msyql; connect to pair.com next

--- db/migrate/20250405001948_create_chrscode.rb
+++
@@ -1,7 +0,0 @@
-class CreateChrscode < ActiveRecord::Migration[7.1]
-  def change
-    create_table :chrscode do |t|
-      t.string :code, limit: 6, null: false
-    end
-  end
-end
--- db/migrate/20250405001953_create_property.rb
+++
@@ -1,7 +0,0 @@
-class CreateProperty < ActiveRecord::Migration[7.1]
-  def change
-    create_table :property do |t|
-      t.string :streetnumberbegin, limit: 16
-    end
-  end
-end
